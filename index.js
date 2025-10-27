5e5580918d07921b422b2b3dbbc90834f35c37cb
ccords-cmd <ccords@anamosa.k12.ia.us>
2025-10-27 13:10:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-var distanceInMiles = 100;
+const distanceInMiles = 100;
 function estimatedArrivalTime(){
   var milesPerHour = 50;
   return () = distanceInMiles/milesPerHour;
 };
 document.getElementById('result').innerHTML = estimatedArrivalTime;
+
 
 
